efd0b800f0de21fd1b481ed9c5ebe6fc6c47ea0f
Rutuja Hulsure <rutuja.hulsure@coditas.com>
2021-12-22 18:56:05 +0530

    Initial Commit

---
+++ NewCustomFootwear-Measurements/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewCustomFootwear-Measurements
+//
+//  Created by Coditas on 22/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
